9568c20609aecc84cdb934a8017cb128b2dbdfe0
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-03-30 23:24:08 +1000

    Services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,39 @@ import { AppComponent } from './app.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
 import {HttpClientModule} from '@angular/common/http';
 import {HammerConfig} from './hammer.config';
+import { PageHeaderComponent } from './view/page-header/page-header.component';
+import { TransportComponent } from './view/transport/transport.component';
 
+// import { SwiperModule } from 'ngx-swiper-wrapper';
+// import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+// import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+import { ServicesComponent } from './view/services/services.component';
 
+// const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+//   direction: 'horizontal',
+//   slidesPerView: 'auto'
+// };
 
 registerLocaleData(ruLocale, 'ru');
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    TransportComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
     AngularSvgIconModule,
-    HttpClientModule
+    HttpClientModule,
+    // SwiperModule
   ],
   providers: [
     {
-      provide: HAMMER_GESTURE_CONFIG,
-      useClass: HammerConfig
+      provide: [HAMMER_GESTURE_CONFIG],
+      useClass: HammerConfig,
+      // useValue: DEFAULT_SWIPER_CONFIG
     }
   ],
   bootstrap: [AppComponent]
