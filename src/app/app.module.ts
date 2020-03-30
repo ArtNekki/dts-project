dd16486f158affde3aee268e518dcef28259baef
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-03-30 22:00:09 +1000

    Project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,24 +7,15 @@ import ruLocale from '@angular/common/locales/ru';
 import { AppComponent } from './app.component';
 import {AngularSvgIconModule} from 'angular-svg-icon';
 import {HttpClientModule} from '@angular/common/http';
-import { TopToolbarComponent } from './view/top-toolbar/top-toolbar.component';
-import { MainNavComponent } from './view/main-nav/main-nav.component';
-import { AlertComponent } from './view/alert/alert.component';
-import { GalleryComponent } from './view/gallery/gallery.component';
 import {HammerConfig} from './hammer.config';
-import { PromoComponent } from './view/promo/promo.component';
+
 
 
 registerLocaleData(ruLocale, 'ru');
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TopToolbarComponent,
-    MainNavComponent,
-    AlertComponent,
-    GalleryComponent,
-    PromoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
