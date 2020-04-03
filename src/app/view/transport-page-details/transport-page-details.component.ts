953bba35050648ac980e4fe0331519a663e8604b
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-04-03 11:54:12 +1000

    Add styles for transport-details

--- src/app/view/transport-page-details/transport-page-details.component.ts
+++ src/app/view/transport-page-details/transport-page-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-transport-page-details',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransportPageDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  close() {
+    this.router.navigate(['/'], { fragment: 'transport' });
   }
 
 }
