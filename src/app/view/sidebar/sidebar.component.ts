d1c1f796b322d7325732edefbc2e0320a80ee72f
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-04-06 12:34:13 +1000

    Header gradient fixed, and services data changed

--- src/app/view/sidebar/sidebar.component.ts
+++ src/app/view/sidebar/sidebar.component.ts
@@ -8,12 +8,59 @@ import {Observable} from 'rxjs';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  list$: Observable<any>;
+  // list$: Observable<any>;
+  list = [
+    {
+      id: 'tippers',
+      name: 'Самосвалы'
+    },
+    {
+      id: 'timber-tracks',
+      name: 'Сортиментовозы'
+    },
+    {
+      id: 'bulldozers',
+      name: 'Бульдозера'
+    },
+    {
+      id: 'gasoline-tankers',
+      name: 'Бензовозы'
+    },
+    {
+      id: 'power-stations',
+      name: 'Электростанции'
+    },
+    {
+      id: 'chambos',
+      name: 'Илососы'
+    },
+    {
+      id: 'low-loader',
+      name: 'Тралы'
+    },
+    {
+      id: 'crawler-excovators',
+      name: 'Эксковаторы гусеничные'
+    },
+    {
+      id: 'auto-towers',
+      name: 'Автовышки'
+    },
+    {
+      id: 'front-loaders',
+      name: 'Погрузчики фронтальные'
+    },
+    {
+      id: 'auto-containers',
+      name: 'Контейнеровозы'
+    }
+  ]
+
 
   constructor(private transportService: TransportService) { }
 
   ngOnInit(): void {
-    this.list$ = this.transportService.getNames();
+    // this.list$ = this.transportService.getNames();
   }
 
 }
