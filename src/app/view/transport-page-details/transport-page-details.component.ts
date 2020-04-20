1090438cad0a277efb9df43c6fe2c3666110f309
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-04-21 06:17:55 +1000

    Logic of transport selection finished

--- src/app/view/transport-page-details/transport-page-details.component.ts
+++ src/app/view/transport-page-details/transport-page-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
@@ -6,11 +6,12 @@ import {Router} from '@angular/router';
   templateUrl: './transport-page-details.component.html',
   styleUrls: ['./transport-page-details.component.scss']
 })
-export class TransportPageDetailsComponent implements OnInit {
+export class TransportPageDetailsComponent implements OnInit, OnChanges {
   @Output() onChange: EventEmitter<any> = new EventEmitter<any>();
   @ViewChild('container', {read: ElementRef}) container: ElementRef;
 
   @Input() transportId;
+  activeClass;
 
   constructor(private router: Router) { }
 
@@ -25,12 +26,22 @@ export class TransportPageDetailsComponent implements OnInit {
   changeData(id: any) {
    this.onChange.emit(id);
    this.transportId = id;
-   this.container.nativeElement.classList.add('transport-details--to-form');
+   this.container.nativeElement.classList.add('transport-details--selected');
   }
 
   goBack() {
-    this.container.nativeElement.classList.remove('transport-details--to-form');
+    this.container.nativeElement.classList.remove('transport-details--selected');
     this.transportId = null;
     this.onChange.emit(this.transportId);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('details changes', changes);
+
+    if (changes.transportId.currentValue) {
+      this.activeClass = 'transport-details--selected';
+    }
+
+    // this.container.nativeElement.classList.add('transport-details--to-form');
   }
 }
