74e014b9019074dfddcd152c483fa39799945997
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-04-21 13:22:52 +1000

    scroll for modals added

--- src/app/view/modal/modal.component.ts
+++ src/app/view/modal/modal.component.ts
@@ -7,6 +7,7 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 })
 export class ModalComponent implements OnInit {
   @Input() title = 'Модальное окно';
+  @Input() cssMod: string;
   @Output() close = new EventEmitter<void>();
 
   constructor() { }
