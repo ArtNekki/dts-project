b24e49c1f8a6bed28c713433fbbf1c45a081c87c
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-04-29 21:20:56 +1000

    Blur added for input in price-list component

--- src/app/view/price-list/price-list.component.ts
+++ src/app/view/price-list/price-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-price-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./price-list.component.scss']
 })
 export class PriceListComponent implements OnInit {
+  @ViewChild('inputTarget', {read: ElementRef}) input: ElementRef;
   isCopied = false;
 
   constructor() { }
@@ -25,6 +26,7 @@ export class PriceListComponent implements OnInit {
   // }
   copied() {
     this.isCopied = true;
+    this.input.nativeElement.blur();
 
     setTimeout(() => {
       this.isCopied = false;
