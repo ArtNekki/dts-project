d04a13af8bfc5f130b4738d239c80f6ea34e488d
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-06-02 16:46:09 +1000

    Page-header refactored

--- src/app/view/page-header/page-header.component.ts
+++ src/app/view/page-header/page-header.component.ts
@@ -5,45 +5,11 @@ import {AfterViewInit, Component, ElementRef, HostListener, InjectionToken, Inpu
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss']
 })
-export class PageHeaderComponent implements OnInit {
-  @Input() transport: HTMLElement;
-  @Input() about: HTMLElement;
-  @Input() features: HTMLElement;
-  @Input() services: HTMLElement;
-  @Input() contacts: HTMLElement;
-  @ViewChild('header', {read: ElementRef}) header: ElementRef;
 
+export class PageHeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
 
   }
-
-  // ngAfterViewInit(): void {
-  //   this.setStickyMenu();
-  // }
-  //
-  // @HostListener('window:scroll', ['$event'])
-  //
-  // scroll(event) {
-  //   this.setStickyMenu();
-  // }
-  //
-  // setStickyMenu() {
-  //   if (this.header.nativeElement.getBoundingClientRect().top < -30) {
-  //     document.documentElement.classList.add('nav-sticky');
-  //   } else {
-  //     document.documentElement.classList.remove('nav-sticky');
-  //   }
-  // }
-  //
-  // openMenu() {
-  //   this.header.nativeElement.classList.add('page-header--menu-opened');
-  //   document.documentElement.classList.add('modal-opened');
-  // }
-  //
-  // closeMenu() {
-  //   this.header.nativeElement.classList.remove('page-header--menu-opened');
-  //   document.documentElement.classList.remove('modal-opened');
-  // }
 }
