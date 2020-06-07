8fc406d0c76d1fe0e9a4c3e15e2d60abb6bd64f2
Anton Mayatskiy <work.mayatskiy@gmail.com>
2020-06-07 18:10:08 +1000

    Modal refactored

--- src/app/view/modal/modal.component.ts
+++ src/app/view/modal/modal.component.ts
@@ -1,34 +1,18 @@
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
-import {animate, style, transition, trigger} from '@angular/animations';
+import {Component, EventEmitter, Input,  OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss'],
-  animations: [
-    trigger('modal', [
-      // transition(':enter', [
-      //   style({opacity: 0}),
-      //   animate(400)
-      // ])
-    ])
-  ]
+  styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit, OnChanges {
+export class ModalComponent implements OnInit {
   @Input() title = 'Модальное окно';
   @Input() cssMod: string;
   @Output() close = new EventEmitter<void>();
-
-  modalState = '';
 
   constructor() { }
 
   ngOnInit(): void {
     this.title = (this.title.indexOf('&') !== -1) ? this.title.split('&').join('<br />') : this.title;
   }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log('changes', changes);
-  }
-
 }
